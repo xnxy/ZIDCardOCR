d03906b9b8fa21ebc6bf6ad95ff002ad997743ba
伟 周 <1661583063@qq.com>
2019-12-06 09:44:06 +0800

    no-message

---
+++ Framework/ZIDCardOCR.framework/Headers/ZIDCardOCR.h
@@ -0,0 +1,20 @@
+//
+//  ZIDCardOCR.h
+//  ZIDCardOCR
+//
+//  Created by CNTP on 2019/11/13.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZIDCardOCR.
+FOUNDATION_EXPORT double ZIDCardOCRVersionNumber;
+
+//! Project version string for ZIDCardOCR.
+FOUNDATION_EXPORT const unsigned char ZIDCardOCRVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZIDCardOCR/PublicHeader.h>
+#import <ZIDCardOCR/ZIDCardOCRManager.h>
+#import <ZIDCardOCR/ZIDCardInfo.h>
+
+
